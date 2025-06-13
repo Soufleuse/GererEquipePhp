1160a80bdf4d81b6b5eca81172ca7ed639b638a0
Steve Lemieux <Steve.Lemieux@mrnf.gouv.qc.ca>
2025-06-13 16:53:54 -0400

    Affichage de la form pour modifier les équipes

---
+++ README.md
@@ -0,0 +1,11 @@
+# Construire l'image
+docker build -t mon-exemple-php .
+
+# Rouler l'image - oui, il y a un user/mot de passe, mais ce sont des données non-nominatives qu'il y a dans la BD.
+docker run -p 12080:12080 -p 8306:3306 -d -v C:\Users\lemst5\source\repos\php\GererEquipePHP\mysql_data:/var/lib/mysql -e MYSQL_DATABASE=LigueHockey -e MYSQL_USER=lemste -e MYSQL_PASSWORD=Misty@00 -e MYSQL_ROOT_PASSWORD=Misty@00 --name mon-appllication mon-exemple-php
+
+# Stopper l'appllication
+docker stop mon-application
+
+# Détruire l'image
+docker rmi mon-exemple-php
--- src/modifierEquipe.php
+++ src/modifierEquipe.php
@@ -1,7 +1,4 @@
-<?php
-include('config.php');
-$page = "modifierEquipe";
-?>
+<?php $page = "modifierEquipe"; ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,10 +7,42 @@ $page = "modifierEquipe";
     <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
-    <div id="wrapAuBoeuf">
-        <?php include('inc/entete.php'); ?>
+    <?php include('inc/entete.php'); ?>
+    <div id="wrap">
         <div id="contenu">
-            <p>En construction...</p>
+            <p>
+                <label for="cboListeEquipe">Équipe à modifier</label>
+                <select id="cboListeEquipe" autofocus required>
+                    <option value="">--- Sélectionnez une équipe ---</option>
+                    <?php
+                        include('inc/accesdonnees.php');
+                        try {
+                            $listeEquipe = ObtenirListeEquipe();
+                            
+                            foreach($listeEquipe as $uneEquipe) {
+                                echo "<option value=\"" . $uneEquipe['id'] . "\">" .
+                                     htmlspecialchars($uneEquipe['nomEquipe']) . " " .
+                                     htmlspecialchars($uneEquipe['ville']) . "</option>";
+                            }
+                        }
+                        catch(Exception $ex) {
+                            echo "<div class=\"erreur\">Une erreur s'est produite lors de la lecture des équipes.</div>";
+                            error_log($ex);
+                        }
+                    ?>
+                </select>
+            </p>
+            <p>
+                <label for="txtNomEquipe">Nom de l'équipe</label>
+                <input type="text" id="txtNomEquipe" name="txtNomEquipe" maxlength="50" placeholder="Nom de l'équipe" required></input>
+            </p>
+            <p>
+                <label for="txtVille">Ville hôte</label>
+                <input type="text" id="txtVille" name="txtVille" maxlength="50" placeholder="Ville hôte" required></input>
+            </p>
+            <p>
+                <input type="submit" id="btnEnregistrer" name="btnEnregistrer" value="Enregistrer" />
+            </p>
         </div>
     </div>
     <?php include('inc/pied.php') ?>
